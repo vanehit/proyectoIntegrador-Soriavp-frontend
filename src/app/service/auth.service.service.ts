382709c1e09562e0072dd3b70f9dc311eef5f0ea
Vanesa Paola Soria <94073289+vanehit@users.noreply.github.com>
2022-11-21 00:56:48 -0300

    commit

--- src/app/service/auth.service.service.ts
+++ src/app/service/auth.service.service.ts
@@ -10,11 +10,9 @@ import { NuevoUsuario } from '../model/nuevo-usuario';
   providedIn: 'root'
 })
 export class AuthService {
-<<<<<<< HEAD
-  authURL = 'https://backend-mgb.herokuapp.com/auth/';
-=======
+
   URL = environment.URL + 'auth/';
->>>>>>> dbccedd5c18384aa757cbb7ab7d9f7f3bc0e7672
+
 
   constructor(private httpClient: HttpClient) { }
 
--- src/app/service/educacion.service.service.ts
+++ src/app/service/educacion.service.service.ts
@@ -8,11 +8,8 @@ import { Educacion } from '../model/educacion';
   providedIn: 'root'
 })
 export class EducacionService {
-<<<<<<< HEAD
-  URL = 'https://backend-mgb.herokuapp.com/educacion/';
-=======
+
   URL = environment.URL + 'educacion/';
->>>>>>> dbccedd5c18384aa757cbb7ab7d9f7f3bc0e7672
 
   constructor(private httpClient : HttpClient) { }
 
